71d7b521779090c7b659f41e198ac14cc5713260
Furkan Aydın <furkan_106_06@hotmail.com>
2020-04-28 16:15:13 +0300

    Koşma, sağa-sola hareket, menü, engeller, restart, level bitirme ve sonraki levele geçme, zemin tasarımı ve kodları hazırlandı. Ses eklenecek, karakter, zemin ve engellerin görünümü değiştirilecek. Pause menüsü tasarlanacak.

---
+++ Assets/0-Scripts/Credits.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class Credits : MonoBehaviour
+{
+    public void BackMenu()
+    {
+        SceneManager.LoadScene("Menu");
+    }
+
+    public void Quit()
+    {
+        Debug.Log("QUIT!");
+        Application.Quit();
+    }
+}
---
+++ Assets/0-Scripts/PlayerMovement.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerMovement : MonoBehaviour
+{
+    public Rigidbody rb;
+    public float forwardForce = 1000f;
+    public float sidewaysForce = 500f;
+
+    
+    void FixedUpdate()
+    {
+        rb.AddForce(0, 0, forwardForce * Time.deltaTime);
+
+        if (Input.GetKey(KeyCode.D))
+        {
+            rb.AddForce(sidewaysForce * Time.deltaTime, 0, 0, ForceMode.VelocityChange);
+        }
+        if (Input.GetKey(KeyCode.A))
+        {
+            rb.AddForce(-sidewaysForce * Time.deltaTime, 0, 0, ForceMode.VelocityChange);
+        }
+        if (rb.position.y <= -1f)
+        {
+            FindObjectOfType<GameManager>().EndGame();
+        }
+    }
+}
